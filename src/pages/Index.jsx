613fc2ce85e631ae322cc8c920316112ef4ab19b
Leonardo Ramirez <angelr521@hotmail.com>
2021-12-10 10:07:33 -0500

    Se agrega slider de imagenes en el index de la aplicacion

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import Carousel from "components/Carousel";
 
 const Index = () => {
   return (
-    <div className="bg-gray-100 h-96">
+    <div className="bg-gray-100 h-100">
       {" "}
-      <h1>React Soft</h1>
+      <h1 className="text-center h1">React Soft</h1>
       {/* <div className="bg-gray-100 h-96">React Soft</div> */}
+      <Carousel />
     </div>
   );
 };
